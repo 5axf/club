828cfdf175c805ee2a577a238161968759555c8d
5axf <46812012+5axf@users.noreply.github.com>
2020-01-15 15:13:29 +0800

    Update WxPayConfig.java

--- src/main/java/com/sky/car/config/WxPayConfig.java
+++ src/main/java/com/sky/car/config/WxPayConfig.java
@@ -2,13 +2,13 @@ package com.sky.car.config;
 
 public class WxPayConfig {
     //小程序appid
-    public static final String appid = "wxdb284d4f007af002";
+    public static final String appid = "appid";
     //微信支付的商户id
-    public static final String mch_id = "1507837931";
+    public static final String mch_id = "mch_id";
     //微信支付的商户密钥
-    public static final String key = "Ty2020TiaNYuSports2clUb2golf2nET";
+    public static final String key = "key";
     //支付成功后的服务器回调url
-    public static final String notify_url = "https://club.tianyusports.com/wx/pay/wxNotify";
+    public static final String notify_url = "https://xxx.com/wx/pay/wxNotify";
     //签名方式
     public static final String SIGNTYPE = "MD5";
     //交易类型
